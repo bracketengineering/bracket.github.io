ef728ed61ad930768ba4b7df6ddb98879bb5c3d3
0xFarted <104228340+0xFadedd@users.noreply.github.com>
2023-03-04 11:58:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-var dropdown = document.getElementById("dropdown");
-var dropdownContent = document.querySelector(".dropdown-content");
+/* Get all drop-down buttons on the page */
+var dropdowns = document.getElementsByClassName("dropdown");
 
-dropdown.addEventListener("click", function() {
-  dropdownContent.classList.toggle("show");
-});
+/* Loop through the drop-down buttons */
+for (var i = 0; i < dropdowns.length; i++) {
+  var dropdown = dropdowns[i];
+  var button = dropdown.getElementsByClassName("dropbtn")[0];
+  var content = dropdown.getElementsByClassName("dropdown-content")[0];
+
+  /* Show the drop-down menu when the button is clicked */
+  button.onclick = function() {
+    content.classList.toggle("show");
+  }
+}
+
